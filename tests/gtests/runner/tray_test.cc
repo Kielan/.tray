e78847d0ec878adc756584e99fca19bb802929b8
Kielan <ki.lemons@gmail.com>
2023-10-06 23:17:36 -0700

    Create tray_test.cc

---
+++ tests/gtests/runner/tray_test.cc
@@ -0,0 +1,6 @@
+/* This file is intentionally left blank. It is necessary for CMake to have a source file for each
+ * executable. However, the tray_tests test runner only comprises of statically linked lib
+ * files, including its main fn.
+ *
+ * See source/tray/kernel/CMakeLists.txt for an example of how to add unit tests to the test
+ * runner. */
